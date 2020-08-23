d64ba347e627e0890835a5cc7e0d00c9b8b97724
Il Harper <afanyiyu@hotmail.com>
2020-08-23 19:31:03 +0800

    feat: Fluent Icon Control Update

--- SimpleBox/Controls/FluentIcon.cs
+++ SimpleBox/Controls/FluentIcon.cs
@@ -16,18 +16,52 @@ namespace SimpleBox.Controls
     {
         private Path _iconDisplay;
 
-        public FluentIcon()
-        {
-            DefaultStyleKey = typeof(FluentIcon);
-        }
+        private int _iconSize = 24;
+
+        public FluentIcon() => DefaultStyleKey = typeof(FluentIcon);
 
         public static readonly DependencyProperty SymbolProperty = DependencyProperty.Register(
-            "Symbol", typeof(string), typeof(FluentIcon), new PropertyMetadata(default(string)));
+            "Symbol", typeof(string), typeof(FluentIcon), new PropertyMetadata("", OnSymbolChanged));
 
         public string Symbol
         {
             get => (string) GetValue(SymbolProperty);
-            set => SetValue(SymbolProperty, value);
+            set
+            {
+                SetValue(SymbolProperty, value);
+                OnSymbolChanged(this, new DependencyPropertyChangedEventArgs());
+            }
+        }
+
+        public static readonly DependencyProperty SizeProperty = DependencyProperty.Register(
+            "Size", typeof(double), typeof(FluentIcon), new PropertyMetadata(1.5, OnSizeChanged));
+
+        private static void OnSizeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (!(d is FluentIcon self) || self._iconDisplay is null) return;
+            double scale = self.Size * (self._iconSize / 24d);
+            self._iconDisplay.SetValue(RenderTransformProperty,
+                new TransformGroup
+                {
+                    Children =
+                    {
+                        new ScaleTransform(scale, scale,
+                            self._iconSize / 2d, self._iconSize / 2d)
+                    }
+                });
+            Thickness padding = (Thickness) self.GetValue(PaddingProperty);
+            self.SetValue(WidthProperty, self._iconSize * scale + padding.Left + padding.Right);
+            self.SetValue(HeightProperty, self._iconSize * scale + padding.Top + padding.Bottom);
+        }
+
+        public double Size
+        {
+            get => (double)GetValue(SizeProperty);
+            set
+            {
+                SetValue(SizeProperty, value);
+                OnSizeChanged(this, new DependencyPropertyChangedEventArgs());
+            }
         }
 
         public override void OnApplyTemplate()
@@ -37,20 +71,22 @@ namespace SimpleBox.Controls
             if (!(GetTemplateChild("IconDisplay") is Path pi)) return;
             _iconDisplay = pi;
             Symbol = Symbol;
+            Size = Size;
         }
 
         private static void OnSymbolChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            if (!(d is FluentIcon self) || !(e.NewValue is string) || self._iconDisplay is null) return;
-            if (FluentIconDataSource.TryGetValue((string)e.NewValue, out string value))
-                self._iconDisplay.Data = Geometry.Parse(value);
+            if (!(d is FluentIcon self) || self.Symbol is null || self._iconDisplay is null) return;
+            if (!FluentIconDataSource.TryGetValue(self.Symbol, out FluentIconSource value)) return;
+            self._iconDisplay.Data = Geometry.Parse(value.Data);
+            self._iconSize = value.Size;
         }
 
         #region Data Source
 
-        private static Dictionary<string, string> _fluentIconDataSource;
+        private static Dictionary<string, FluentIconSource> _fluentIconDataSource;
 
-        private static Dictionary<string, string> FluentIconDataSource
+        private static Dictionary<string, FluentIconSource> FluentIconDataSource
         {
             get
             {
@@ -58,12 +94,26 @@ namespace SimpleBox.Controls
 
                 string data = Application.GetResourceStream(new Uri("pack://application:,,,/Assets/Icons.json"))?.Stream.ReadToEnd();
                 if (!string.IsNullOrEmpty(data))
-                    _fluentIconDataSource = JsonConvert.DeserializeObject<Dictionary<string, string>>(data);
+                    _fluentIconDataSource = JsonConvert.DeserializeObject<Dictionary<string, FluentIconSource>>(data);
 
                 return _fluentIconDataSource;
             }
         }
 
         #endregion
+    }
+
+    [JsonObject(MemberSerialization.OptIn)]
+    internal class FluentIconSource
+    {
+        [JsonProperty]
+        private string data = "";
+
+        public string Data => data;
+
+        [JsonProperty]
+        private int size = 24;
+
+        public int Size => size;
     }
 }
