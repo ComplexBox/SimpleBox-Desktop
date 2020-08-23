29cbac401c83ca1cbe78f431433d974f976c6e9f
Il Harper <afanyiyu@hotmail.com>
2020-08-23 13:06:00 +0800

    feat: Add FluentWindow in MainWindow

--- SimpleBox/Windows/MainWindow.xaml.cs
+++ SimpleBox/Windows/MainWindow.xaml.cs
@@ -17,7 +17,7 @@ namespace SimpleBox.Windows
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
