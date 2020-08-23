c432e48c284e9842dee1991208084bdf30376a04
Il Harper <afanyiyu@hotmail.com>
2020-08-24 00:17:05 +0800

    feat: Add AppVersion Property in SettingsWindow

--- SimpleBox/Windows/SettingsWindow.DataContext.cs
+++ SimpleBox/Windows/SettingsWindow.DataContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,10 @@ namespace SimpleBox.Windows
 {
     public partial class SettingsWindow
     {
+        #region About
+
+        public string AppVersion => $"版本 {Assembly.GetExecutingAssembly().GetName().Version}";
+
+        #endregion
     }
 }
