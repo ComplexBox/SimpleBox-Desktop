7766d9970e3acbce0a48c7802035e7f6eb66b386
Il Harper <afanyiyu@hotmail.com>
2020-08-23 14:26:23 +0800

    feat: Config Class Update

--- SimpleBox/Models/Config.cs
+++ SimpleBox/Models/Config.cs
@@ -1,27 +1,78 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using SimpleBox.Helpers;
 
 namespace SimpleBox.Models
 {
-    public sealed class Config
+    [JsonObject(MemberSerialization.OptIn)]
+    public sealed class Config : INotifyPropertyChanged
     {
         #region Current
 
-        public static Config Current { get; set; }
+        public static Config Current { get; } = ConfigHelper.OpenConfig();
 
         #endregion
 
         #region Update
 
+        [JsonProperty]
         private string updateServer = "https://simplebox.vbox.moe/stable";
 
         public string UpdateServer
         {
             get => updateServer;
-            set => updateServer = value;
+            set
+            {
+                updateServer = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region User Data
+
+        [JsonProperty]
+        private string domain = "";
+
+        public string Domain
+        {
+            get => domain;
+            set
+            {
+                domain = value;
+                OnPropertyChanged();
+            }
+        }
+
+        [JsonProperty]
+        private string password = "";
+
+        public string Password
+        {
+            get => password;
+            set
+            {
+                password = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region Property Changed
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
         #endregion
