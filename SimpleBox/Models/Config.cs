443f89f0e8fcfcb88beeaf281654ea89032a3e33
Il Harper <afanyiyu@hotmail.com>
2020-08-23 13:35:52 +0800

    feat: Add Config Model

---
+++ SimpleBox/Models/Config.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleBox.Models
+{
+    public sealed class Config
+    {
+        #region Current
+
+        public static Config Current { get; set; }
+
+        #endregion
+
+        #region Update
+
+        private string updateServer = "https://simplebox.vbox.moe/stable";
+
+        public string UpdateServer
+        {
+            get => updateServer;
+            set => updateServer = value;
+        }
+
+        #endregion
+    }
+}
