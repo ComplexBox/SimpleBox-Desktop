ae3388d78cc23566efe99599393ae8c4854d0aaa
Il Harper <afanyiyu@hotmail.com>
2020-08-23 14:23:08 +0800

    feat: Add Config Helper

---
+++ SimpleBox/Helpers/ConfigHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using SimpleBox.Models;
+
+namespace SimpleBox.Helpers
+{
+    public static class ConfigHelper
+    {
+        public static string GetConfigFolder()
+        {
+            string folder = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @"SimpleBox");
+            Directory.CreateDirectory(folder);
+            return folder;
+        }
+
+        private static string GetConfigFileName() => Path.Combine(GetConfigFolder(), "config.json");
+
+        public static Config OpenConfig()
+        {
+            if (!File.Exists(GetConfigFileName())) return new Config();
+            return JsonConvert.DeserializeObject<Config>(File.ReadAllText(GetConfigFileName()));
+        }
+
+        public static void SaveConfig(Config config)
+        {
+            File.WriteAllText(
+                GetConfigFileName(), JsonConvert.SerializeObject(config));
+        }
+    }
+}
