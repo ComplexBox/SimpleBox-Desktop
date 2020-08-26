104f082621ec8d1e4ff4d04e47dac6ff0f8b81f9
Il Harper <afanyiyu@hotmail.com>
2020-08-26 22:40:08 +0800

    feat: DateTimeUtils Class Update

--- SimpleBox/Utils/DateTimeUtils.cs
+++ SimpleBox/Utils/DateTimeUtils.cs
@@ -10,93 +10,39 @@ namespace SimpleBox.Utils
     {
         private const long InitialJavaScriptDateTicks = 621355968000000000;
 
-        private static TimeSpan GetUtcOffset(this DateTime d) => TimeZoneInfo.Local.GetUtcOffset(d);
-
-        private static long ToUniversalTicks(DateTime dateTime) =>
-            dateTime.Kind == DateTimeKind.Utc
-                ? dateTime.Ticks
-                : ToUniversalTicks(dateTime, dateTime.GetUtcOffset());
-
-        private static long ToUniversalTicks(DateTime dateTime, TimeSpan offset)
-        {
-            if (dateTime.Kind == DateTimeKind.Utc || dateTime == DateTime.MaxValue || dateTime == DateTime.MinValue)
-                return dateTime.Ticks;
-            long num = dateTime.Ticks - offset.Ticks;
-            if (num > 3155378975999999999L)
-                return 3155378975999999999;
-            return num < 0L ? 0L : num;
-        }
-
         internal static long ConvertDateTimeToJavaScriptTicks(DateTime dateTime, bool convertToUtc = true)
         {
-            return UniversalTicksToJavaScriptTicks(convertToUtc ? ToUniversalTicks(dateTime) : dateTime.Ticks);
+            TimeSpan offset = TimeZoneInfo.Local.GetUtcOffset(dateTime);
+            long ret;
+            if (dateTime.Kind == DateTimeKind.Utc || dateTime == DateTime.MaxValue || dateTime == DateTime.MinValue)
+                ret = dateTime.Ticks;
+            else
+            {
+                long num = dateTime.Ticks - offset.Ticks;
+                if (num > 3155378975999999999L)
+                    ret = 3155378975999999999;
+                else
+                {
+                    ret = num < 0L ? 0L : num;
+                }
+            }
+
+            return ((convertToUtc ? dateTime.Kind == DateTimeKind.Utc
+                ? dateTime.Ticks
+                : ret : dateTime.Ticks) - InitialJavaScriptDateTicks) / 10000L;
         }
 
-        private static long UniversalTicksToJavaScriptTicks(long universalTicks)
-        {
-            return (universalTicks - InitialJavaScriptDateTicks) / 10000L;
-        }
-
-        private static DateTime ConvertJavaScriptTicksToDateTime(long javaScriptTicks)
-        {
-            return new DateTime(javaScriptTicks * 10000L + InitialJavaScriptDateTicks, DateTimeKind.Utc);
-        }
-
-        public static bool TryGetDateFromConstructorJson(
+        internal static bool TryGetDateFromConstructorJson(
             JsonReader reader,
             out DateTime dateTime)
         {
             dateTime = new DateTime();
-            if (!TryGetDateConstructorValue(reader, out var integer1) || !integer1.HasValue)
+
+            if (!reader.Read() || reader.TokenType != JsonToken.Integer || reader.Value == null)
                 return false;
 
-            if (!TryGetDateConstructorValue(reader, out var integer2))
-                return false;
-            if (integer2.HasValue)
-            {
-                var longList = new List<long>
-                {
-                    integer1.Value,
-                    integer2.Value
-                };
-                while (TryGetDateConstructorValue(reader, out var integer3))
-                {
-                    if (integer3.HasValue)
-                        longList.Add(integer3.Value);
-                    else
-                    {
-                        if (longList.Count > 7)
-                            return false;
+            dateTime = new DateTime((long)reader.Value * 10000L + InitialJavaScriptDateTicks, DateTimeKind.Utc);
 
-                        while (longList.Count < 7)
-                            longList.Add(0L);
-                        dateTime = new DateTime((int) longList[0], (int) longList[1] + 1,
-                            longList[2] == 0L ? 1 : (int) longList[2], (int) longList[3], (int) longList[4],
-                            (int) longList[5], (int) longList[6]);
-                        goto label_16;
-                    }
-                }
-
-                return false;
-            }
-
-            dateTime = ConvertJavaScriptTicksToDateTime(integer1.Value);
-            label_16:
-            return true;
-        }
-
-        private static bool TryGetDateConstructorValue(
-            JsonReader reader,
-            out long? integer)
-        {
-            integer = new long?();
-            if (!reader.Read())
-                return false;
-            if (reader.TokenType == JsonToken.EndConstructor)
-                return true;
-            if (reader.TokenType != JsonToken.Integer)
-                return false;
-            if (reader.Value != null) integer = (long) reader.Value;
             return true;
         }
     }
