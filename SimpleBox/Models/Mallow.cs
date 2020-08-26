89b3a1944d310348eaab456af75d84af1d176de7
Il Harper <afanyiyu@hotmail.com>
2020-08-26 18:53:33 +0800

    feat: Mallow Class Update

--- SimpleBox/Models/Mallow.cs
+++ SimpleBox/Models/Mallow.cs
@@ -11,7 +11,7 @@ using Newtonsoft.Json;
 namespace SimpleBox.Models
 {
     [JsonObject(MemberSerialization.OptIn)]
-    public class Mallow : INotifyPropertyChanged
+    public class Mallow : INotifyPropertyChanged, IModified
     {
         #region Const Data
 
@@ -75,13 +75,60 @@ namespace SimpleBox.Models
             }
         }
 
+        [JsonProperty]
+        private long createTime;
+
+        public long OriginalCreateTime
+        {
+            get => createTime;
+            set
+            {
+                createTime = value;
+                OnPropertyChanged();
+            }
+        }
+
+        [JsonProperty]
+        private long responseTime;
+
+        public long OriginalResponseTime
+        {
+            get => responseTime;
+            set
+            {
+                responseTime = value;
+                OnPropertyChanged();
+            }
+        }
+
         #endregion
 
         #region Local Data Context
 
         private bool _isLocal;
 
-        public bool IsLocal => _isLocal;
+        public bool IsLocal
+        {
+            get => _isLocal;
+            private set
+            {
+                _isLocal = value;
+                OnPropertyChanged();
+            }
+        }
+
+        [JsonProperty]
+        private bool modified;
+
+        public bool Modified
+        {
+            get => modified;
+            set
+            {
+                modified = value;
+                OnPropertyChanged();
+            }
+        }
 
         [JsonProperty]
         private string localMsg = "";
@@ -92,6 +139,7 @@ namespace SimpleBox.Models
             set
             {
                 localMsg = value;
+                Modified = true;
                 OnPropertyChanged();
             }
         }
@@ -105,6 +153,33 @@ namespace SimpleBox.Models
             set
             {
                 localResponse = value;
+                Modified = true;
+                OnPropertyChanged();
+            }
+        }
+
+        [JsonProperty]
+        private long localCreateTime;
+
+        public long LocalCreateTime
+        {
+            get => localCreateTime;
+            set
+            {
+                localCreateTime = value;
+                OnPropertyChanged();
+            }
+        }
+
+        [JsonProperty]
+        private long localResponseTime;
+
+        public long LocalResponseTime
+        {
+            get => localResponseTime;
+            set
+            {
+                localResponseTime = value;
                 OnPropertyChanged();
             }
         }
@@ -116,10 +191,15 @@ namespace SimpleBox.Models
         [OnDeserialized]
         private void SetValuesOnDeserialized(StreamingContext context)
         {
+            IsLocal = Id == LocalOnlyId;
             if (string.IsNullOrEmpty(LocalMessage))
                 LocalMessage = OriginalMessage;
             if (string.IsNullOrEmpty(localResponse))
                 LocalResponse = OriginalResponse;
+            if (LocalCreateTime == 0)
+                LocalCreateTime = OriginalCreateTime;
+            if (LocalResponseTime == 0)
+                LocalResponseTime = OriginalResponseTime;
         }
 
         #endregion
