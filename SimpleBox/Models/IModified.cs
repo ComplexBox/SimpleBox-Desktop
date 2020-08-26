19c9146620231f2474308759222cb19b7aed199e
Il Harper <afanyiyu@hotmail.com>
2020-08-26 18:08:34 +0800

    feat: add IModified Interface

---
+++ SimpleBox/Models/IModified.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleBox.Models
+{
+    public interface IModified
+    {
+        public bool Modified { get; set; }
+    }
+}
