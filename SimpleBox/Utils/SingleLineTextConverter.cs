e7fa9a4cdac8fd2d0cd1ff7a5ce68ef567c08d24
Il Harper <afanyiyu@hotmail.com>
2020-08-28 23:58:15 +0800

    feat: Add SingleLineTextConverter

---
+++ SimpleBox/Utils/SingleLineTextConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace SimpleBox.Utils
+{
+    public class SingleLineTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is null) return string.Empty;
+            string s = (string) value;
+            s = s.Replace(Environment.NewLine, " ");
+            return s;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+            throw new NotImplementedException();
+    }
+}
