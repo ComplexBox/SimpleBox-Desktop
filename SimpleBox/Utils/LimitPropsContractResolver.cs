b4546d1982aa64de577e9bac86613db77b191fbf
Il Harper <afanyiyu@hotmail.com>
2020-08-28 16:38:26 +0800

    feat: Add LimitPropsContractResolver Class

---
+++ SimpleBox/Utils/LimitPropsContractResolver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace SimpleBox.Utils
+{
+    public sealed class LimitPropsContractResolver : DefaultContractResolver
+    {
+        private readonly string[] _props;
+
+        public LimitPropsContractResolver(string[] props) => _props = props;
+
+        protected override IList<JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization) =>
+            base.CreateProperties(type, memberSerialization).Where(p => _props.Contains(p.PropertyName))
+                .ToList();
+    }
+}
