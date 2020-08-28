9dfef02f0c400450c166f956e1c217939d565f80
Il Harper <afanyiyu@hotmail.com>
2020-08-29 00:40:24 +0800

    feat: Add WebPush Class

---
+++ SimpleBox/Core/WebPush.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleBox.Core
+{
+    public sealed class WebPush
+    {
+        #region Current
+
+        public static WebPush Current;
+
+        #endregion
+    }
+}
