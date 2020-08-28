17806ad8c4323ba6a14039cc063adb5c01a3b18a
Il Harper <afanyiyu@hotmail.com>
2020-08-28 19:10:30 +0800

    feat: Add RenameWindow

---
+++ SimpleBox/Windows/RenameWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SimpleBox.Windows
+{
+    /// <summary>
+    /// RenameWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class RenameWindow
+    {
+        private RenameWindow()
+        {
+            InitializeComponent();
+        }
+
+        public static string Rename(string origin)
+        {
+            RenameWindow window = new RenameWindow
+            {
+                RenameTextBox =
+                {
+                    Text = origin
+                }
+            };
+            window.ShowDialog();
+            return window.RenameTextBox.Text;
+        }
+
+        private void ApplyButtonClick(object sender, RoutedEventArgs e) => Close();
+
+        private void CancelButtonClick(object sender, RoutedEventArgs e)
+        {
+            RenameTextBox.Text = "";
+            Close();
+        }
+    }
+}
