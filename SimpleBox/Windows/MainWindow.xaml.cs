c6b1f304397ebfc3f28430f0258d6f11125ca9df
Il Harper <afanyiyu@hotmail.com>
2020-08-28 15:56:16 +0800

    feat: Add MainWindow CaptionBar Hook

--- SimpleBox/Windows/MainWindow.xaml.cs
+++ SimpleBox/Windows/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
@@ -35,15 +36,21 @@ namespace SimpleBox.Windows
             };
 
             Closed += (sender, args) => Application.Current.Shutdown(0);
+
+            Loaded += OnLoaded;
         }
 
         #endregion
 
         #region Event Triggers
 
-        private void OpenSettingsButtonClick(object sender, RoutedEventArgs e) => SettingsWindow.ShowSettings();
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            IntPtr hwnd = new WindowInteropHelper(this).Handle;
+            HwndSource.FromHwnd(hwnd).AddHook(WndProc);
+        }
 
-        #endregion
+        private void OpenSettingsButtonClick(object sender, RoutedEventArgs e) => SettingsWindow.ShowSettings();
 
         private void CreateMallowClick(object sender, RoutedEventArgs e)
         {
@@ -52,5 +59,26 @@ namespace SimpleBox.Windows
             MallowSource.CurrentSource.Current.Mallows.Add(mallow);
             MallowSource.CurrentSource.Current.CurrentMallow = mallow;
         }
+
+        #endregion
+
+        #region WndProc
+
+        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            if (msg != 0x0084) return IntPtr.Zero;
+            Point p = new Point();
+            int pInt = lParam.ToInt32();
+            p.X = (pInt << 16) >> 16;
+            p.Y = pInt >> 16;
+            Point rel = WndTitleArea.PointFromScreen(p);
+            bool inside = PointFromScreen(p).Y <= 32 || rel.X >= 0 && rel.X <= WndTitleArea.ActualWidth && rel.Y >= 0 &&
+                          rel.Y <= WndTitleArea.ActualHeight;
+            if (!inside) return IntPtr.Zero;
+            handled = true;
+            return new IntPtr(2);
+        }
+
+        #endregion
     }
 }
