4969b24a51a8090cfaad092370c2af515751f4e1
Il Harper <afanyiyu@hotmail.com>
2020-09-06 19:18:27 +0800

    feat: Add Puller Class

---
+++ SimpleBox/Core/Puller.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleBox.Models;
+
+namespace SimpleBox.Core
+{
+    public interface IMallowPuller : IMallowProvider
+    {
+        public Mallow[] Pull(UserPass userPass, int mode);
+    }
+
+    public static class PullHelper
+    {
+
+    }
+}
