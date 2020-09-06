c456cb2103a4ea9cfb1367d9789ed5ceedf1e120
Il Harper <afanyiyu@hotmail.com>
2020-09-06 22:37:31 +0800

    feat: Add MainWindow DataContext

---
+++ SimpleBox/Windows/MainWindow.DataContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleBox.Windows
+{
+    public partial class MainWindow : INotifyPropertyChanged
+    {
+        #region Data Context
+
+        private bool _isSelecting;
+
+        public bool IsSelecting
+        {
+            get => _isSelecting;
+            set
+            {
+                _isSelecting = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region Property Changed
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
