7bdca323eceec328507ccb92160b5d4ed5aeb1c4
Il Harper <afanyiyu@hotmail.com>
2020-09-09 22:10:24 +0800

    feat: Add PullWindow

--- SimpleBox/Puller/Puller.cs
+++ SimpleBox/Puller/Puller.cs
@@ -5,16 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 using SimpleBox.Core;
 using SimpleBox.Models;
+using SimpleBox.Utils.State;
 
 namespace SimpleBox.Puller
 {
     public interface IMallowPuller : IMallowProvider
     {
-
+        public Progress Progress { get; }
     }
 
     public static class PullHelper
     {
+        public static void Pull(IMallowPuller puller)
+        {
 
+        }
     }
 }
---
+++ SimpleBox/Windows/PullWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using SimpleBox.Puller;
+using SimpleBox.Utils.State;
+
+namespace SimpleBox.Windows
+{
+    /// <summary>
+    /// PullWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class PullWindow
+    {
+        #region Constructors
+
+        public PullWindow(IMallowPuller puller)
+        {
+            // Create Progress for Display
+
+            Progress = new Progress(puller.Progress, Dispatcher);
+
+            // DataContext Initialize
+
+            PullerName = puller.Name;
+
+            // Initialize Component
+
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Data Context
+
+        public string PullerName { get; }
+
+        public Progress Progress { get; }
+
+        #endregion
+    }
+}
