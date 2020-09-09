0444e500c081055c99b89719118a414714852328
Il Harper <afanyiyu@hotmail.com>
2020-09-09 21:37:14 +0800

    refactor: Move Puller Class to Puller

--- SimpleBox/Puller/Puller.cs
+++ SimpleBox/Puller/Puller.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SimpleBox.Core;
 using SimpleBox.Models;
 
-namespace SimpleBox.Core
+namespace SimpleBox.Puller
 {
     public interface IMallowPuller : IMallowProvider
     {
