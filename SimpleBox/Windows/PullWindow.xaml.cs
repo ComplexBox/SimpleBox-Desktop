1f90eea5091d21e5b02abb8cb1edcdccaca65358
Il Harper <afanyiyu@hotmail.com>
2020-09-10 00:36:12 +0800

    feat: PullWindow Complete

--- SimpleBox/Windows/PullWindow.xaml.cs
+++ SimpleBox/Windows/PullWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,6 +14,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using SimpleBox.Models;
 using SimpleBox.Puller;
 using SimpleBox.Utils.State;
 
@@ -19,11 +23,11 @@ namespace SimpleBox.Windows
     /// <summary>
     /// PullWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class PullWindow
+    public partial class PullWindow : INotifyPropertyChanged
     {
         #region Constructors
 
-        public PullWindow(MallowPuller puller)
+        public PullWindow(MallowPuller puller, EventWaitHandle handle)
         {
             // Create Progress for Display
 
@@ -32,6 +36,9 @@ namespace SimpleBox.Windows
             // DataContext Initialize
 
             PullerName = puller.Name;
+            CreateGroupName = $"{puller.Name}导入";
+
+            Handle = handle;
 
             // Initialize Component
 
@@ -40,11 +47,82 @@ namespace SimpleBox.Windows
 
         #endregion
 
+        #region Core
+
+        private EventWaitHandle Handle;
+
+        #endregion
+
         #region Data Context
 
         public string PullerName { get; }
 
         public Progress Progress { get; }
+
+        private MallowGroup _selectedGroup;
+
+        public MallowGroup SelectedGroup
+        {
+            get => _selectedGroup;
+            set
+            {
+                _selectedGroup = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _createGroupName;
+
+        public string CreateGroupName
+        {
+            get => _createGroupName;
+            set
+            {
+                _createGroupName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _isCreateMode;
+
+        public bool IsCreateMode
+        {
+            get => _isCreateMode;
+            set
+            {
+                _isCreateMode = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region Event Processors
+
+        private void StartClick(object sender, RoutedEventArgs e)
+        {
+            RootElement.IsEnabled = false;
+            Handle?.Set();
+        }
+
+        private void CancelClick(object sender, RoutedEventArgs e) => Close();
+
+        private void ModeChangeClick(object sender, RoutedEventArgs e)
+        {
+            RadioButton radio = sender as RadioButton;
+            if (radio?.Tag != null) IsCreateMode = radio.Tag as string == "CreateModeRadioButton";
+        }
+
+        #endregion
+
+        #region Property Changed
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
 
         #endregion
     }
