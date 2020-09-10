d0436a5ba1a6670249e9e141c5f08a32f652820e
Il Harper <afanyiyu@hotmail.com>
2020-09-10 21:09:50 +0800

    feat: Add CookieHelper Class

---
+++ SimpleBox/Utils/Cef/CookieHelper.cs
@@ -0,0 +1,14 @@
+﻿// https://github.com/crifan/crifanLib/blob/master/csharp/crifanLib.cs
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleBox.Utils.Cef
+{
+    public static class CookieHelper
+    {
+    }
+}
