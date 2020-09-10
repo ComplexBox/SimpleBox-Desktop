5ffb83eb3dac3e5fac7933d0e95678a67108e505
Il Harper <afanyiyu@hotmail.com>
2020-09-10 15:12:05 +0800

    feat: PullHelper Complete

--- SimpleBox/Puller/PullHelper.cs
+++ SimpleBox/Puller/PullHelper.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
 using SimpleBox.Core;
 using SimpleBox.Models;
 using SimpleBox.Utils.State;
@@ -31,6 +33,45 @@ namespace SimpleBox.Puller
             bool isCreateMode = pullWindow.IsCreateMode;
             MallowGroup selectedMallowGroup = pullWindow.SelectedGroup;
             string createGroupName = pullWindow.CreateGroupName;
+
+            puller.Progress.IsIndeterminate = true;
+            puller.Progress.Text = $"初始化{puller.Name}服务……";
+
+            Mallow[] mallows = puller.VerifyAndPull(isCreateMode ? selectedMallowGroup.Mallows.ToArray() : Array.Empty<Mallow>());
+
+            pullWindow.Close();
+
+            if (mallows is null)
+                return;
+
+            if (mallows.Length == 0)
+            {
+                MessageBox.Show(
+                    "没有要拉取的项目。",
+                    "成功",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Information,
+                    MessageBoxResult.OK);
+
+                return;
+            }
+
+            Dispatcher.CurrentDispatcher.Invoke(() =>
+            {
+                MallowGroup resultGroup = isCreateMode
+                    ? new MallowGroup
+                    {
+                        Name = createGroupName
+                    }
+                    : selectedMallowGroup;
+                if (resultGroup is null) return;
+
+                for (int i = 0; i < mallows.Length; i++)
+                    resultGroup.Mallows.Insert(i, mallows[i]);
+
+                MallowSource.CurrentSource.Data.Insert(0, resultGroup);
+                MallowSource.CurrentSource.Current = resultGroup;
+            });
         }
     }
 }
